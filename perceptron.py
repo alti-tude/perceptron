a3eee3075a5e993584a8aa840a31c4197efb2afb
alti-tude <kartik.gupta0204@gmail.com>
2018-12-01 22:26:32 +0530

    forward propagation vectorised(loops over outpus only not test cases)

--- perceptron.py
+++ perceptron.py
@@ -12,15 +12,26 @@ def plot_image(x):
     plt.show()
 
 
-def initialise(dims):
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+
+
+def initialise(dims, x, y):
     """
         dims contains the dimensions of the layers where the first layer is the input layer(direct inputs) 
         and the last is the output
 
         w[i] -> wt from (i-1)th layer to the ith layer
         b[i] -> b for the ith layer
+
+        x_flat -> (dims[0] x m)
+        y_expanded -> (k x m)
     """
     l = len(dims)
+    m = len(x)
+    k = dims[-1]
+    
+    ############# w and b ###########################
     b = [[0]]
     w = [[0]]
     np.random.seed(5)
@@ -30,58 +41,53 @@ def initialise(dims):
         b.append(bt)
         w.append(wt)
     
-    return w, b
+    ############ x_flat ##############################
+    x_flat = np.zeros( (m, dims[0]) ) #take transpose later for proper x_flat (dim[0]->ip layer  x  m)
 
+    for i in range(m):
+        batch = x[i].reshape(1, x[i].shape[0]*x[i].shape[1])
+        x_flat[i] = batch
+    
+    x_flat = x_flat.T
 
-def sigmoid(x):
-    return 1/(1+np.exp(-x))
+    ######### y_cur ##################################
+    y_expanded = np.zeros( (dims[-1], m) )
+    
+    for i in range(m):
+        y_expanded[y[i]][i] = 1
+
+    return w, b, x_flat, y_expanded ,l , m, k
 
 
 def forward_propagation(l, x, w, b):
     """
         l -> number of layers(including input)
-        x -> current test case (img_x * img_y * 1, 1)
+        x -> test case (img_x * img_y * 1, m)
         w -> wts (layer l, n[l-1], n[l])
+
+        return a[-1] (k x m)
     """
     a = [x]
     for i in range(1, l):
         z = w[i].T @ a[i-1] + b[i]
         a.append(sigmoid(z))
-    return a
+    return a[-1]
 
 
-def cost(yhat, y):
-    y = y.reshape(y.shape[0], 1)
-    yhat = yhat.reshape(yhat.shape[0], 1)
-    return -(y.T @ np.log(yhat) + (1-y).T @ np.log(1-yhat))[0][0]
-
-
-def loss(l, x, y, w, b):
+def loss(yhat, y, k, m):
     """
-        m -> number of training examples
-        x -> the raw_training input (m, imgx, imgy)
-        y -> is the training output (m, 1)
-        w -> (l, dim[i-1], dim[i])
-        b -> (l, dim[i], 1)
-    """
-    m = len(x)
-    j = 0
-
-    for i in range(m):
-        batch = x[i]
-        x_flat = batch.reshape(batch.shape[0]*batch.shape[1], 1)
-        a = forward_propagation(l, x_flat, w, b)
+        iterate over all the outputs and take effective dot product (@ takes internal dot)
+        -(y[i] @ np.log(yhat[i]) + (1-y[i]) @ np.log(1-yhat[i])) -> cost
         
-        y_cur = np.zeros( (dims[-1], 1) )
-        y_cur[y[i]] = 1
-        j += cost(a[-1], y_cur)
+        yhat -> k x m
+        y -> k x m
 
-        done = int(i/len(x)*100)
-        print('#'*done + '_'*(100-done), end="\r")
-   
-    print("")
-    print(j/m)
-    return j/m
+        return int
+    """
+    loss = 0
+    for i in range(k):
+        loss += -(y[i] @ np.log(yhat[i]) + (1-y[i]) @ np.log(1-yhat[i]))
+    return loss/m
 
 
 if __name__ == '__main__':
@@ -90,13 +96,22 @@ if __name__ == '__main__':
     x_train, x_test = x_train, x_test
     
     dims = [28*28, 10, 9, 10]
-    w, b = initialise(dims)
-    m = len(x_train)
-
+    w, b, x_flat, y_expanded, l, m, k = initialise(dims, x_train, y_train)
+    print(x_flat.shape)
+    print(x_train.shape)
+    
+    print(y_expanded.shape)
+    yhat = forward_propagation(l, x_flat, w, b)
+    print(loss(yhat, y_expanded, k, m))
     #cost function test
-    # y = np.array([1, 0, 0, 1])
-    # yhat = np.array([0.5, 0.1, 0.2, 0.001])
+    # y = np.array([[0, 0, 0, 0], [0, 0, 0, 0]])
+    # y = y.T 
+    # yhat = np.array([[0.5,0.5,0.5,0.5], [0.5,0.5,0.5,0.5]])
+    # yhat = yhat.T
+    # print(yhat)
+    # print(cost(yhat, y))
+
     # assert(abs(cost(yhat, y)[0][0] - 7.92940653) < 1e10)
     
-    loss(4, x_train, y_train, w, b)
+    # loss(4, x_train, y_train, w, b)
     
