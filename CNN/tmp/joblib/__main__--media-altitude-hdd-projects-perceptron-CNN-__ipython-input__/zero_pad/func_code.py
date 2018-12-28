3586ef6342bec1695ef050a8b962e13110a5e900
alti-tude <kartik.gupta0204@gmail.com>
2018-12-28 21:54:21 +0530

    added CNN

--- CNN/tmp/joblib/__main__--media-altitude-hdd-projects-perceptron-CNN-__ipython-input__/zero_pad/func_code.py
+++
@@ -1,9 +0,0 @@
-# first line: 1
-@mem.cache
-def zero_pad(x, pad):
-    return np.pad(
-            x, 
-            ((0,0), (pad, pad), (pad, pad), (0,0)),
-            'constant', 
-            constant_values = (0)
-        )
